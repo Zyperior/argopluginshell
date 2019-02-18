2d1cba975bc9c117fed8281d9cc77d8fdd10308a
Andreas <albihn.andreas@gmail.com>
2019-02-18 16:38:54 +0100

    Added annotation + javadoc-comment

--- src/mypackage/MyPluginMainClass.java
+++ src/mypackage/MyPluginMainClass.java
@@ -1,7 +1,12 @@
 package mypackage;
 
 import http.server.serviceloader.ARGOPlugin;
+import http.server.serviceloader.PluginType;
 
+/**
+ * PluginType-tag should be what the html object sends as URL in HTTP-request.
+ */
+@PluginType("tag")
 public class MyPluginMainClass implements ARGOPlugin {
 
     @Override
