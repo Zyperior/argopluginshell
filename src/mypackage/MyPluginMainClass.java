58c805dc2aa1d5a8823c1b5f692b9c1e184c407b
Andreas <albihn.andreas@gmail.com>
2019-02-18 16:34:05 +0100

    first commit

---
+++ src/mypackage/MyPluginMainClass.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+import http.server.serviceloader.ARGOPlugin;
+
+public class MyPluginMainClass implements ARGOPlugin {
+
+    @Override
+    public void doSomething(String doThis, String[] params) {
+
+    }
+}
